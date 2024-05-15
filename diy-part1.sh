2b170ad84fd537418df86e33346b85bd8d7d3d3f
Your Name <you@example.com>
2024-05-15 12:24:41 +0800

    version

---
+++ diy-part1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Add a feed source
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
